4993f070b0a48d552ebe88c102d52e91e5d24309
Odin Thif <mjd.thif@icloud.com>
2024-09-11 16:47:48 +0200

    seperate of concerns signin | signup

--- src/controllers/signup/signup.ts
+++ src/controllers/signup/signup.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
-// import { prisma } from "../server";
-import { hashSync, compareSync } from "bcryptjs";
+import { hashSync } from "bcryptjs";
 import { validationResult } from "express-validator";
-import * as jwt from "jsonwebtoken";
-import { db } from "../dbschema/db"; // Your Drizzle db instance
-import { users } from "../dbschema/schema"; // The users schema definition from Drizzle
 import { eq } from "drizzle-orm/expressions";
-import { signUpSchema, signInSchema } from "../dbschema/zod-validations";
+import { signUpSchema } from "../../dbschema/zod-validations";
+import { users } from "../../dbschema/schema";
+import { db } from "../../dbschema/db";
 
 // -=-=-=-=-=-=- signup controller
 
@@ -56,49 +54,5 @@ export const signup = async (req: Request, res: Response) => {
     }
   } else {
     res.status(400).send("The input in not valid!");
-  }
-};
-
-// -=-=-=-=-=-=-=-== login controller
-export const login = async (req: Request, res: Response) => {
-  const userValidated = signInSchema.safeParse(req.body);
-  if (userValidated.success) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-
-      const { email, password } = userValidated.data;
-
-      // Find user by email
-      const user = await db.select().from(users).where(eq(users.email, email));
-
-      if (user.length === 0) {
-        return res.status(400).json({ message: "User does not exist!" });
-      }
-
-      // Check password
-      const validPassword = compareSync(password, user[0].password);
-      if (!validPassword) {
-        return res.status(400).json({ message: "Incorrect password!" });
-      }
-
-      // Generate JWT token
-      const token = jwt.sign({ userId: user[0].id }, process.env.SECRET_KEY!, {
-        expiresIn: "1h", // Set token expiration time as needed
-      });
-
-      res.status(200).json({
-        message: "User logged in successfully",
-        user: user[0], // Drizzle returns an array, so use the first item
-        token: token,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  } else {
-    res.status(400).send("Please check your Email or Password!");
   }
 };
--- src/router/userRouter/userRouter.ts
+++ src/router/userRouter/userRouter.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { login, signup } from "../../controllers/auth";
+import { signup } from "../../controllers/signup/signup";
+import { login } from "../../controllers/signin/signin";
+
 const userRouter: express.Router = express.Router();
 
 userRouter.get("/", (req: Request, res: Response) => {
