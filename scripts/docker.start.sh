8f45bf219ce4108fd82c68ad1d28d8e28b59d56e
Odin Thif <mjd.thif@icloud.com>
2024-09-12 01:18:22 +0200

    dev merge

--- scripts/docker.start.sh
+++ scripts/docker.start.sh
@@ -1,1 +1,2 @@
-./node_modules/.bin/prisma migrate deploy && node build/server.js
+# ./node_modules/.bin/prisma migrate deploy && node build/server.js
+./node_modules/.bin/drizzle-kit migrate && node build/server.js
