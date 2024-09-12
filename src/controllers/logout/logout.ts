1ede5ae94b78603c2793a3228fd6566645a208eb
Odin Thif <mjd.thif@icloud.com>
2024-09-12 20:20:41 +0200

    added logout

--- src/controllers/logout/logout.ts
+++ src/controllers/logout/logout.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-
+//-=-=-=-=-=-=-= Logout | removing the cookies from the res.
 export const logout = async (req: Request, res: Response) => {
   res.clearCookie("token");
   res.status(200).json({ success: true, message: "Log out successfully" });
